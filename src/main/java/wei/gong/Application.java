9025c8f8834485b8e01395b943217764d6a2d97d
wei <liuweichinash@163.com>
2017-11-17 11:38:18 +0800

    create project

---
+++ src/main/java/wei/gong/Application.java
@@ -0,0 +1,11 @@
+package wei.gong;
+
+/**
+ * @author wei.liu
+ * Created with IntelliJ IDEA.
+ * Description:
+ * Date: 2017-11-17
+ * Time: 11:35
+ */
+public class Application {
+}
---
+++ src/main/java/wei/gong/service/Che315Service.java
@@ -0,0 +1,11 @@
+package wei.gong.service;
+
+/**
+ * @author wei.liu
+ * Created with IntelliJ IDEA.
+ * Description:
+ * Date: 2017-11-17
+ * Time: 11:36
+ */
+public class Che315Service {
+}
