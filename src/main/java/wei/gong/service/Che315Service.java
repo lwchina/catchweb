feefc6ebac09f0acbe87369a89988349617ea7ca
lwchina <liuweichinash@163.com>
2017-11-24 17:38:27 +0800

    test

--- src/main/java/wei/gong/service/Che315Service.java
+++ src/main/java/wei/gong/service/Che315Service.java
@@ -1,5 +1,13 @@
 package wei.gong.service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * @author wei.liu
  * Created with IntelliJ IDEA.
@@ -9,7 +17,34 @@ package wei.gong.service;
  */
 public class Che315Service {
 
-    public static void main(String[] args) {
+    private static final int OK = 200;
+
+    public static void main(String[] args) throws IOException {
         System.out.println("test");
+        String baseUrl = "http://www.315che.com";
+        //String baseUrl = "http://www.baidu.com";
+        first(baseUrl);
+    }
+
+    public static void first (String baseUrl) throws IOException {
+        URL url = new URL(baseUrl);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+        httpURLConnection.setRequestMethod("GET");
+        httpURLConnection.setConnectTimeout(2000);
+        httpURLConnection.setReadTimeout(2000);
+
+        int code = httpURLConnection.getResponseCode();
+        if ( code == OK ) {
+            InputStream responseInputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(responseInputStream, "UTF-8"));
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        }
+
+
     }
 }
